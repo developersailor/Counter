d3fce12c18dee2191625d96fb33bb2b3b2d0a3ab
Mehmet Fıșkındal <developersailor@gmail.com>
2023-07-30 10:59:11 +0300

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Mehmet Fışkındal on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
